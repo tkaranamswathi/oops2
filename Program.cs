4e31cfd61c9b6c7642356c8714ad1b1b301f6424
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-14 11:57:35 +0530

    swathi

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UC5PATTERN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int rows = 8;
+
+            for (int i = rows; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
