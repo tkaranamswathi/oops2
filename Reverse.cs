6f94057a206d676df04f1df21dbf12c8890697c4
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-14 12:11:43 +0530

    swathi

---
+++ Reverse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC7REVERSE
+{
+     class Reverse
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World! How are you?";
+
+            string reversedString = ReverseWords(input);
+
+            Console.WriteLine("Reversed string: " + reversedString);
+        }
+
+        static string ReverseWords(string input)
+        {
+            string[] words = input.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
+
