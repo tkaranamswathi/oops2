bcb318bc48d3865c851d2d9ce3291e9d92feda63
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-14 12:06:27 +0530

    swathi

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace UC6PATTERN2
+{
+    internal class Program
+    {
+   
+            static void Main(string[] args)
+            {
+                int rows = 7;
+                int cols = 7;
+
+                for (int i = 1; i <= rows; i++)
+                {
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        if (i == 1 || i == rows || j == 1 || j == cols)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        
+    }
+    
+}
