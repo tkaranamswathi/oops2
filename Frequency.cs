82778b9d50f83b59532e52aadbd3166fbe930eaf
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-14 11:36:22 +0530

    swathi

---
+++ Frequency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3FREQUENCY
+{
+    class Frequency
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 2, 3, 5, 6, 1 };
+
+            Dictionary<int, int> frequency = CountElementFrequency(numbers);
+
+            Console.WriteLine("Element Frequency:");
+
+            foreach (var kvp in frequency)
+            {
+                Console.WriteLine("Element: " + kvp.Key + ", Frequency: " + kvp.Value);
+            }
+        }
+
+        static Dictionary<int, int> CountElementFrequency(int[] numbers)
+        {
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+            foreach (int num in numbers)
+            {
+                if (frequency.ContainsKey(num))
+                {
+                    frequency[num]++;
+                }
+                else
+                {
+                    frequency[num] = 1;
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
+
