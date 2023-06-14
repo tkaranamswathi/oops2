cbeb3c8a7bf3d7dc8bbcf632bcdcece73fe28852
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-14 12:17:21 +0530

    swathi

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UC8SUM
+{
+    class sumofdigits
+    {
+        static void Main(String[] args)
+        {
+
+            int a = 567, sum = 0, b;
+
+            
+            while (a != 0)
+            {
+                b = a % 10;
+                sum = sum + b; 
+                a = a / 10; 
+            }
+            Console.WriteLine("The sum of the digits is: " + sum);
+        }
+    }
+}
