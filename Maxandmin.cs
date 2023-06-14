6c10b97f32e7f7cca5252e847095f453f9ceefa4
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-14 11:48:38 +0530

    swathi

---
+++ Maxandmin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4MAXANDMIN
+{
+    class Maxandmin
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 2, 9, 1, 7, 4 };
+
+            int maximum = FindMaximum(numbers);
+            int minimum = FindMinimum(numbers);
+
+            Console.WriteLine("Maximum element: " + maximum);
+            Console.WriteLine("Minimum element: " + minimum);
+        }
+
+        static int FindMaximum(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("The array is empty.");
+            }
+
+            int maximum = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maximum)
+                {
+                    maximum = numbers[i];
+                }
+            }
+
+            return maximum;
+        }
+
+        static int FindMinimum(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("The array is empty.");
+            }
+
+            int minimum = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < minimum)
+                {
+                    minimum = numbers[i];
+                }
+            }
+
+            return minimum;
+        }
+    }
+}
+
